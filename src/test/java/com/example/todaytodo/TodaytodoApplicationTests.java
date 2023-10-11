dda2021e2e8f3a86f4d60afd3eb70496b7bfb384
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-10-11 17:32:27 +0900

    set project

---
+++ src/test/java/com/example/todaytodo/TodaytodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todaytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaytodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
