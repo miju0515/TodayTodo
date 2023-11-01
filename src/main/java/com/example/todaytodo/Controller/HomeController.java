44e049cee48bde60777c255ad8ff4305813f0ec4
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-11-01 15:13:13 +0900

    feat: 초기 화면 불러올 때 회원 정보 만들기
    원활한 테스트를 위해 초기 화면을 불러왔을 때 유저를 생성하게 하였다.

--- src/main/java/com/example/todaytodo/Controller/HomeController.java
+++ src/main/java/com/example/todaytodo/Controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.example.todaytodo.Controller;
 
+import com.example.todaytodo.Service.SiteUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final SiteUserService siteUserService;
+
     @GetMapping("/")
     public String home(){
+        siteUserService.add();
         return "home";
     }
 }
